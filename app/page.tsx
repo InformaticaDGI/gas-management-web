d8cb14732ef138c0b27224e3b37380e99b25564f
Salvador Ron <salvadorronosio@gmail.com>
2025-11-08 23:54:38 -0400

    add codegen and constants

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
-import Image from "next/image";
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
+import createApolloClient from "@/graphql-client";
+import { CountriesDocument } from "@/graphql/generated/graphql";
 
-export default function Home() {
+const getCountries = async () => {
+  const client = createApolloClient();
+  const response = await client.query({
+    query: CountriesDocument
+  })
+
+  return response
+
+}
+
+export default async function Home() {
+  const { data, error } = await getCountries();
+  if(!data) return "Loading...";
+  if(error) return "Ha ocurrido un error";
+  console.log()
+
   return (
     <div>
+      {data.countries.map(country => (
+        <p key={country.code}>{country.name} {country.emoji}</p>
+      ))}
       <Button>Click Me!</Button>
     </div>
   );
