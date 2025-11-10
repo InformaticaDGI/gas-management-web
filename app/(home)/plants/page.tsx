ce217aeb4bcf3a2809c872a0c732ce7ba82e69c0
Salvador Ron <salvadorronosio@gmail.com>
2025-11-10 17:00:50 -0400

    fix plant

--- app/(home)/plants/page.tsx
+++ app/(home)/plants/page.tsx
@@ -1,3 +1,10 @@
+import { PlantDataTable } from "@/components/plant-datatable";
+
+
 export default function PlantsPage() {
-    return <div>PlantsPage</div>
+    return <div>
+        <div className="w-full max-w-full py-4">
+            <PlantDataTable data={[]} />
+        </div>
+    </div>
 }
