f5c8e9c4ed557353234af89a97bbe68c1a53be85
Salvador Ron <salvadorronosio@gmail.com>
2025-11-10 16:20:12 -0400

    fix layout

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,29 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+export default function Layout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return <SidebarProvider
+    className="hidden md:flex"
+    style={
+      {
+        "--sidebar-width": "calc(var(--spacing) * 64)",
+        "--header-height": "calc(var(--spacing) * 12 + 1px)",
+      } as React.CSSProperties
+    }
+  >
+    <AppSidebar variant="sidebar" />
+    <SidebarInset>
+      <SiteHeader />
+      <div className="flex flex-1 flex-col">
+        <div className="@container/main flex flex-1 flex-col gap-2">
+          {children}
+        </div>
+      </div>
+    </SidebarInset>
+  </SidebarProvider>
+}
