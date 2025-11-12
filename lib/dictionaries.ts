991eaafbb915222ac45af71ef9c0d323bf3909d4
Salvador Ron <salvadorronosio@gmail.com>
2025-11-12 14:53:56 -0400

    reorder libs

---
+++ lib/dictionaries.ts
@@ -0,0 +1,16 @@
+export const plantDictionaryNames: Record<string, string> = {
+    code: "Código",
+    name: "Nombre",
+    address: "Dirección",
+    phone: "Teléfono",
+    email: "Correo"
+  }
+
+  export const userDictionaryNames: Record<string, string> = {
+    name: "Nombre",
+    email: "Correo",
+    role: "Rol",
+    status: "Estado",
+    userPlants: "Plantas",
+    "size-plants": "Cantidad"
+  }
